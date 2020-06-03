a8091098e2b45fb2bd960597d2f93d84c0202d31
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2020-06-02 21:05:16 -0400

    Added some tests, added .gitignore, added Rectangle, added some modifiers

--- src/com/company/model/shape/Posn.java
+++ src/com/company/model/shape/Posn.java
@@ -1,5 +1,7 @@
 package com.company.model.shape;
 
+import java.util.Objects;
+
 public class Posn {
   public final int x;
   public final int y;
@@ -7,5 +9,19 @@ public class Posn {
   public Posn(int x, int y) {
     this.x = x;
     this.y = y;
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (!(other instanceof Posn)) {
+      return false;
+    }
+    Posn posn = (Posn) other;
+    return x == posn.x && y == posn.y;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(x, y);
   }
 }
---
+++ test/com/company/model/shape/AShapeTest.java
@@ -0,0 +1,72 @@
+package com.company.model.shape;
+
+import com.company.model.shape.shapes.Ellipse;
+import com.company.model.shape.shapes.Rectangle;
+
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A class to test AShape and its implementations.
+ */
+public class AShapeTest {
+  AShape rect1;
+  AShape rect2;
+  AShape ellipse1;
+  AShape ellipse2;
+
+  private void initTestData() {
+    this.rect1 = new Rectangle(new Posn(3, 5), 5, 6, Color.BLUE);
+    this.rect2 = new Rectangle(new Posn(17, 2), 4, 3, Color.GREEN);
+    this.ellipse1 = new Ellipse(new Posn(10, 10), 5, 5, Color.RED);
+    this.ellipse2 = new Ellipse(new Posn(20, 5), 7, 3, Color.MAGENTA);
+  }
+
+  @Test
+  public void getPosn() {
+    this.initTestData();
+    assertEquals(new Posn(3, 5), rect1.getPosition());
+    assertEquals(new Posn(17, 2), rect2.getPosition());
+    assertEquals(new Posn(10, 10), ellipse1.getPosition());
+    assertEquals(new Posn(20, 5), ellipse2.getPosition());
+  }
+
+  @Test
+  public void getColor() {
+    this.initTestData();
+    assertEquals(Color.BLUE, rect1.getColor());
+    assertEquals(Color.GREEN, rect2.getColor());
+    assertEquals(Color.RED, ellipse1.getColor());
+    assertEquals(Color.MAGENTA, ellipse2.getColor());
+  }
+
+  @Test
+  public void getShapeType_customConstructors() {
+    this.initTestData();
+    assertEquals(ShapeType.Rectangle, rect1.getShapeType());
+    assertEquals(ShapeType.Rectangle, rect2.getShapeType());
+    assertEquals(ShapeType.Ellipse, ellipse1.getShapeType());
+    assertEquals(ShapeType.Ellipse, ellipse2.getShapeType());
+  }
+
+  @Test
+  public void getWidth() {
+    this.initTestData();
+    assertEquals(5, rect1.getWidth());
+    assertEquals(4, rect2.getWidth());
+    assertEquals(5, ellipse1.getWidth());
+    assertEquals(7, ellipse2.getWidth());
+  }
+
+  @Test
+  public void getHeight() {
+    this.initTestData();
+    assertEquals(6, rect1.getHeight());
+    assertEquals(3, rect2.getHeight());
+    assertEquals(5, ellipse1.getHeight());
+    assertEquals(3, ellipse2.getHeight());
+  }
+}
